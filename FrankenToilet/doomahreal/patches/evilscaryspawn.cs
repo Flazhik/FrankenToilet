d219e69b1bd402e9d01c93ab96bf4f8fc30ef30c
Minepool9 <97184060+Minepool9@users.noreply.github.com>
2026-01-27 15:36:02 +0200

    feat(evil scary, pande and dozer)
    whoops should had added them as seperate commits

---
+++ FrankenToilet/doomahreal/patches/evilscaryspawn.cs
@@ -0,0 +1,31 @@
+﻿using FrankenToilet.Core;
+using HarmonyLib;
+using System;
+using System.Xml.Linq;
+using UnityEngine;
+using Object = UnityEngine.Object;
+using Random = UnityEngine.Random;
+
+namespace FrankenToilet.doomahreal.patches;
+
+[PatchOnEntry]
+[HarmonyPatch]
+public static class Startlevelpatch
+{
+    private static int _evilStartCounter = 0;
+
+    [HarmonyPatch(typeof(PlayerTracker), "LevelStart")]
+    [HarmonyPostfix]
+    public static void Postfix()
+    {
+        _evilStartCounter++;
+        LogHelper.LogInfo($"Evil Scary Round Start Counter: {_evilStartCounter}");
+        if (_evilStartCounter < 6) return; //This is jank, level loading calls this twice and i want it to happen after 3 times, Fuck you hakita
+
+        var bundle = IMLOADINGITSOHARDDDD.thegrundle;
+        GameObject? prefab = bundle.LoadAsset<GameObject>("Assets/Custom/imfrakeninmykill/evilscary/round start canvas/EvilScary 1.prefab");
+            
+        Object.Instantiate(prefab);
+        _evilStartCounter = 0;
+    }
+}
